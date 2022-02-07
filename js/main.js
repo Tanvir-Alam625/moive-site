d6c42e201a00d0325860455146ffe817090d5735
tanveer <mdtanviralamht731@gmail.com>
2022-02-07 22:14:43 +0600

    main.js file added

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// html element selection
+// const navigationButton = document.getElementById('nav_btn'); 
+const navigationItems = document.getElementById('navigation'); 
+function myFunction(){
+    navigationItems.classList.toggle('hidden');
+}
